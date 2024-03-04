2c7acbc1747d2660b78cc1fe69639aa76de4e481
tusharm03 <muppidi.tushar@gmail.com>
2024-03-04 15:59:27 -0700

    librarires

--- facial_rec.py
+++ facial_rec.py
@@ -4,20 +4,20 @@ import numpy as np
 
 #Face Structue
 
-# cap = cv2.VideoCapture(0)
-# facecascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-# while cap.isOpened():
-#     ret,frame = cap.read()
-#     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-#     faces = facecascade.detectMultiScale(gray,1.3,5)
-#     for a in faces:
-#         cv2.rectangle(frame,(a[0],a[1]),(a[0]+a[2],a[1]+a[3]),(0,255,255), 2)
-#     cv2.imshow("Video",frame)
+cap = cv2.VideoCapture(0)
+facecascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+while cap.isOpened():
+    ret,frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    faces = facecascade.detectMultiScale(gray,1.3,5)
+    for a in faces:
+        cv2.rectangle(frame,(a[0],a[1]),(a[0]+a[2],a[1]+a[3]),(0,255,255), 2)
+    cv2.imshow("Video",frame)
 
-#     if cv2.waitKey(1) == ord("q"):
-#         break
-# cap.release()
-# cv2.destroyAllWindows()
+    if cv2.waitKey(1) == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()
 
 ######################################################
 
